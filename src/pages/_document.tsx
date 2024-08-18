18fdbb708f011dee0f05e8324faaae0f68ddaad0
rizky-verandi <classieroof@gmail.com>
2024-08-18 18:04:26 +0700

    test commit _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
-      <body className="dark">
+      <Head />
+      <body className={`overflow-y-hidden`}>
         <Main />
         <NextScript />
       </body>
