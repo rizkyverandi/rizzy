c95da94b37d5a7f25f5582a691ccf4785be99ec3
rizky-verandi <classieroof@gmail.com>
2024-11-01 19:43:53 +0700

    add a cats library project and fix font family

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,12 @@
 import { Quicksand } from "next/font/google";
 import { ReactLenis } from "lenis/react";
 import dynamic from "next/dynamic";
-import Head from "next/head";
 import { GlobalProvider } from "@/contexts";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 //const inter = Pixelify_Sans({ subsets: ["latin"] });
-const inter = Quicksand({ subsets: ["latin"] });
+const quicksand = Quicksand({ subsets: ["latin"] });
 
 // Dynamically import the component
 const Loader = dynamic(() => import("./Loader"));
@@ -23,13 +22,13 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <GlobalProvider>
-        <Loader className={`p-4 z-10 ${inter.className}`} />
+        <Loader className={`p-4 z-10 ${quicksand.className}`} />
         <ReactLenis root>
-          <Navbar className={`${inter.className}`} />
-          <main className={`${inter.className} p-4 text-cta-text relative z-0`}>
+          <Navbar className={`${quicksand.className}`} />
+          <main className={`${quicksand.className} p-4 text-cta-text relative z-0`}>
             {children}
           </main>
-          <Footer className={inter.className} />
+          <Footer className={quicksand.className} />
           {/* {loadSecond && <Navbar className={`${inter.className}`} />}
           
           {loadSecond && (
